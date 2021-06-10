f571f9aa50137fb1aec7943eb37092496fa64936
Fernando <fercoche369@gmail.com>
2021-06-09 19:15:35 -0600

    Update

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include <OddParityExample.h>
+#include <Sender.h>
+#include <Receiver.h>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     OddParityExample oddParityExample = OddParityExample();
     oddParityExample.sender(2);
+
+//    Sender sender = Sender();
+//    sender.operation();
+//
+//    Receiver receiver = Receiver();
+//    receiver.operation();
+
     return 0;
 }
