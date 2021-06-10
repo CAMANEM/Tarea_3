2abaed6bc8d4b76730173aad2a43844ee8723c88
Fernando <fercoche369@gmail.com>
2021-06-09 18:48:21 -0600

    HP

---
+++ Longitudinal Redundancy/L_Checker.h
@@ -0,0 +1,20 @@
+//
+// Created by camanem on 9/6/21.
+//
+
+#ifndef TAREA_3_L_CHECKER_H
+#define TAREA_3_L_CHECKER_H
+
+
+#include <string>
+
+class L_Checker {
+
+public:
+    L_Checker();
+    std::string getString(char x);
+    void Init();
+};
+
+
+#endif //TAREA_3_L_CHECKER_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
+//
+// Created by fernando on 9/6/21.
+//
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include <L_Checker.h>
+
+int main(){
+
+    L_Checker hp = L_Checker();
+    hp.Init();
+
     return 0;
 }
